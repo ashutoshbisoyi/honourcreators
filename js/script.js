b4a3882be435ceeb4fc942e5e77398415c71b07f
Ashutosh Bisoyi <62354832+ashutoshbisoyi@users.noreply.github.com>
2020-06-15 20:03:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//To remove the animation of featured img from mobile
 $(document).ready(function () {
     $(window).on("resize", function (e) {
         checkScreenSize();
@@ -5,12 +6,25 @@ $(document).ready(function () {
 
     checkScreenSize();
 
-    function checkScreenSize(){
+    function checkScreenSize() {
         var newWindowWidth = $(window).width();
+        //if the width is less than 600(Mobile Screen)
         if (newWindowWidth < 600) {
-            $(".feature").removeClass("animate__animated");
+            $(".feature").removeClass("animate__animated");//remove animation of featured img
+            $(".backToTop").css('display', 'none');//hide backToTop button
+            $(".whyUs .col-md-3").attr('data-aos', 'fade-in');//to avoid screen drop in whyus section
         }
     }
+});
+//To animate the backToTop button in scroll
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 50) {
+            $(".backToTop").css('bottom', '3%');
+        } else {
+            $(".backToTop").css('bottom', '-15%');
+        }
+    });
 });
 
 
